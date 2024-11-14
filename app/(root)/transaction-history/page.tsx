893544eb42ef24c3adaab27509b813393ef5f20a
ChristineWu <chw081@ucsd.edu>
2024-11-14 10:59:46 -0800

    Revert "fix"
    This reverts commit 8958a400146763ed86d43a974a238e7730631571.

--- app/(root)/transaction-history/page.tsx
+++ app/(root)/transaction-history/page.tsx
@@ -1,24 +1,8 @@
-import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
-const TransactionHistory = ({ searchParams: {id, page}}: SearchParamProps) => {
+const TransactionHistory = () => {
   return (
-    <div className="transactions">
-      <div className="transactions-header">
-        <HeaderBox 
-          title="Transaction History"
-          subtext="See your bank details and transactions."
-        />
-      </div>
-
-      <div className="space-y-6">
-        <div className="transactions-account">
-          <div className="flex flex-col gap-2">
-            <h2></h2>
-          </div>
-        </div>
-      </div>
-    </div>
+    <div>TransactionHistory</div>
   )
 }
 
